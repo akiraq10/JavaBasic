8a91627c94746083f769c2f6f9a2a4bc92bd2084
kkk-home <xuankhoi999@gmail.com>
2020-12-20 08:51:12 +0700

    add  HasMap class

--- src/test/java/basic/HasMap.java
+++ src/test/java/basic/HasMap.java
@@ -1,7 +1,9 @@
 package basic;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class HasMap {
     public static void main(String[] args) {
@@ -11,7 +13,17 @@ public class HasMap {
         map.put(3,"xyz");
         System.out.println(map.get(1));
         System.out.println(map.get(2));
-        System.out.println(map.get((3)));
         System.out.println(map.size());
+
+        Set<Integer> idMap=map.keySet();
+        for(int id:idMap){
+            System.out.println(id);
+        }
+
+        Collection nameValue=map.values();
+        for(Object name:map.values()){
+            System.out.println(name.toString());
+        }
+
     }
 }
